d311719f54ed010b688b6a09281f5d427cad26e5
Stephen-Kamau <stiveckamash@gmail.com>
2020-05-23 21:59:07 -0400

    modified views

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,13 @@
 # from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .serializers import ChatSerializer
+from .models import Chat
+
+
+class ChatViewSet(viewsets.ModelViewSet):
+    '''creating view functions'''
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,3 +1,18 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializers import ThreadSerializer , ForumSerializer
+from .models import Forum , Thread
+
+
+class ThreadViewSet(viewsets.ModelViewSet):
+
+    queryset = Thread.object.all()
+    serializer_class = ThreadSerializer
+
+
+class ForumViewSet(viewsets.ModelViewSet):
+
+    queryset = Forum.object.all()
+    serializer_class = ForumSerializer
