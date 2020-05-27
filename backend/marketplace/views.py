f244b17f3c0585d6889b3dd007a6904a6f3f7b11
dengsang <dengsangt@gmail.com>
2020-05-27 18:54:06 +0300

    feat! Adding viewsets for the serialized objects

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -1,8 +1,8 @@
 """Views module focuses on API views."""
 
 from rest_framework import viewsets
-from .models import ProductsAds
-from .serializers import ProductsAdsSerializers
+from .models import ProductsAds, Category, Pricing, Reviews
+from .serializers import ProductsAdsSerializers, CategorySerializer, PricingSerializer, ReviewsSerializer
 
 
 class ProductsAdsView(viewsets.ModelViewSet):
@@ -10,3 +10,24 @@ class ProductsAdsView(viewsets.ModelViewSet):
 
     queryset = ProductsAds.objects.all()
     serializer_class = ProductsAdsSerializers
+
+
+class CategoryView(viewsets.ModelViewSet):
+    """Category Views."""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PricingViews(viewsets.ModelViewSet):
+    """Pricing Views."""
+
+    queryset = Pricing.objects.all()
+    serializer_class = PricingSerializer
+
+
+class ReviewsView(viewsets.ModelViewSet):
+    """Reviews Views."""
+
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
